094a8d7aa0a33017ecea7006d4491f2646ef0002
Enrico Pilz <kontakt@enricopilz.de>
2019-11-25 22:11:37 +0100

    Use double linked list for black areas

--- src/main/java/de/enricopilz/constraints/solver/NonogramSolver.java
+++ src/main/java/de/enricopilz/constraints/solver/NonogramSolver.java
@@ -20,36 +20,116 @@ public class NonogramSolver {
      * - Anfang ist mindestens bei 0 (outerLeft), höchstens bei 2 (innerLeft)
      * - Ende ist mindestens vor 3 (innerRight), höchstens vor 5 (outerRight)
      */
-    private int[] nu;
-    private int[] outerLeft;
-    private int[] innerLeft;
-    private int[] innerRight;
-    private int[] outerRight;
+    static class BlackArea {
+
+        private final int length;
+
+        private int outerLeft;
+        private int outerRight;
+
+        private BlackArea left;
+        private BlackArea right;
+
+        BlackArea(final int length) {
+            this.length = length;
+            outerLeft = Integer.MIN_VALUE;
+            outerRight = Integer.MAX_VALUE;
+        }
+
+        public void setLeft(final BlackArea left) {
+            this.left = left;
+        }
+
+        public void setRight(final BlackArea right) {
+            this.right = right;
+        }
+
+        public int getOuterLeft() {
+            return outerLeft;
+        }
+
+        public int getOuterRight() {
+            return outerRight;
+        }
+
+        public int getInnerLeft() {
+            return outerRight - length;
+        }
+
+        public void setInnerLeft(int innerLeft) {
+            outerRight = innerLeft + length;
+        }
+
+        public int getInnerRight() {
+            return outerLeft + length;
+        }
+
+        public void setInnerRight(int innerRight) {
+            outerLeft = innerRight - length;
+        }
+
+        public void setMinimalOuterLeft(int newOuterLeft) {
+            if (newOuterLeft <= outerLeft) {
+                return;
+            }
+            outerLeft = newOuterLeft;
+            if (right != null) {
+                right.setMinimalOuterLeft(outerLeft + length + 1);
+            }
+        }
+
+        public void setMaximalOuterRight(int newOuterRight) {
+            if (newOuterRight >= outerRight) {
+                return;
+            }
+            outerRight = newOuterRight;
+            if (left != null) {
+                left.setMaximalOuterRight(outerRight - length - 1);
+            }
+        }
+
+        public int getLength() {
+            return length;
+        }
+    }
+
+    // Zahlen sind in einer doppelt verketteten Liste hinterlegt (pro Linie), Alternative: LinkedList
+    private BlackArea[] bas;
+
     private char[] output;
 
     public String solveLine(String input, int[] numbers) {
-        this.nu = numbers;
-        outerLeft = new int[numbers.length];
-        innerLeft = new int[numbers.length];
-        innerRight = new int[numbers.length];
-        outerRight = new int[numbers.length];
+        this.bas = new BlackArea[numbers.length];
         this.output = input.toCharArray();
+        for (int n = 0; n < numbers.length; n++) {
+            bas[n] = new BlackArea(numbers[n]);
+        }
+        for (int n = 0; n < numbers.length; n++) {
+            if (n > 0) {
+                bas[n].setLeft(bas[n - 1]);
+            }
+            if (n < numbers.length - 1) {
+                bas[n].setRight(bas[n + 1]);
+            }
+        }
         return String.valueOf(solveLine());
     }
 
     public char[] solveLine() {
         char[] tmp;
+        System.out.println("Urzustand:");
         printState();
+        System.out.println("Initialization:");
         useTechniqueSimpleBoxes();
         printState();
         do {
             tmp = output.clone();
             useTechniqueForcing();
             useTechniqueGlueing();
-            useTechniqueMercury();
 
             setDefiniteBlack();
             setDefiniteWhite();
+            System.out.println("Loop:");
             printState();
         } while (!Arrays.equals(tmp, output));
 
@@ -58,63 +138,51 @@ public class NonogramSolver {
 
     private void printState() {
         System.out.println("'" + String.valueOf(output) + "'");
-        for (int n = 0; n < nu.length; n++) {
-            System.out.println("index: " + n + ", zahl: " + nu[n] + "," +
-                    " von " + outerLeft[n] + "/" + innerLeft[n] +
-                    " bis " + innerRight[n] + "/" + outerRight[n]);
+        for (BlackArea ba : bas) {
+            System.out.println("zahl: " + ba.length + "," +
+                    " von " + ba.getOuterLeft() + "/" + ba.getInnerLeft() +
+                    " bis " + ba.getInnerRight() + "/" + ba.getOuterRight());
         }
         System.out.println();
     }
 
+    // initialization
     public void useTechniqueSimpleBoxes() {
-        if (nu.length == 0) {
+        if (bas.length == 0) {
             return;
         }
-        // untergrenzen
-        outerLeft[0] = 0;
-        innerRight[0] = outerLeft[0] + nu[0];
-        for (int n = 1; n < nu.length; n++) {
-            outerLeft[n] = outerLeft[n - 1] + nu[n - 1] + 1;
-            innerRight[n] = outerLeft[n] + nu[n];
-        }
-        // obergrenzen
-        outerRight[nu.length - 1] = output.length;
-        innerLeft[nu.length - 1] = outerRight[nu.length-1] - nu[0];
-        for (int n = nu.length - 2; n >= 0; n--) {
-            outerRight[n] = outerRight[n + 1] - nu[n + 1] - 1;
-            innerLeft[n] = outerRight[n] - nu[n];
-        }
+        bas[0].setMinimalOuterLeft(0);
+        bas[bas.length-1].setMaximalOuterRight(output.length);
     }
 
     public void useTechniqueForcing() {
-        for (int n = 0; n < nu.length; n++) {
-            increaseFirstPossibleStart(n);
-            decreaseLastPossibleEnding(n);
+        for (BlackArea ba : bas) {
+            increaseFirstPossibleStart(ba);
+            decreaseLastPossibleEnding(ba);
         }
     }
 
-    private void increaseFirstPossibleStart(int n) {
-        for (int start = outerLeft[n]; start <= innerLeft[n]; start++) {
-            if (isPossibleStart(n, start)) {
-                outerLeft[n] = start;
-                innerRight[n] = outerLeft[n] + nu[n];
+    private void increaseFirstPossibleStart(BlackArea ba) {
+        // TODO Optimierungsmöglichkeiten
+        for (int start = ba.getOuterLeft(); start <= ba.getInnerLeft(); start++) {
+            if (isPossibleStart(ba, start)) {
+                ba.setMinimalOuterLeft(start);
                 return;
             }
         }
     }
 
-    private void decreaseLastPossibleEnding(int n) {
-        for (int end = outerRight[n]; end >= innerRight[n]; end--) {
-            if (isPossibleEnd(n, end)) {
-                outerRight[n] = end;
-                innerLeft[n] = outerRight[n] - nu[n];
+    private void decreaseLastPossibleEnding(BlackArea ba) {
+        for (int end = ba.getOuterRight(); end >= ba.getInnerRight(); end--) {
+            if (isPossibleEnd(ba, end)) {
+                ba.setMaximalOuterRight(end);
                 return;
             }
         }
     }
 
-    private boolean isPossibleStart(int n, int start) {
-        for (int i = start; i < start + nu[n]; i++) {
+    private boolean isPossibleStart(BlackArea ba, int start) {
+        for (int i = start; i < start + ba.getLength(); i++) {
             if (output[i] == WHITE) {
                 return false;
             }
@@ -122,9 +190,9 @@ public class NonogramSolver {
         return true;
     }
 
-    private boolean isPossibleEnd(int n, int end) {
-        for (int i = end; i > end - nu[n]; i--) {
-            if (output[i-1] == WHITE) {
+    private boolean isPossibleEnd(BlackArea ba, int end) {
+        for (int i = end; i > end - ba.getLength(); i--) {
+            if (output[i - 1] == WHITE) {
                 return false;
             }
         }
@@ -132,64 +200,55 @@ public class NonogramSolver {
     }
 
     private void useTechniqueGlueing() {
-        // von unten - TODO noch nicht präzise definiert
-        for (int n = 0; n < nu.length; n++) {
-            for (int i = outerLeft[n]; i < innerLeft[n]; i++) {
-                if (output[i] == BLACK) {
-                    innerLeft[n] = i;
-                    outerRight[n] = i + nu[n];
-                }
-            }
+        for (BlackArea ba : bas) {
+            glueingFromLeft(ba);
         }
-        // von oben - TODO noch nicht präzise definiert
-        for (int n = nu.length - 1; n >= 0; n--) {
-            for (int i = outerRight[n]; i > innerRight[n]; i--) {
-                if (output[i - 1] == BLACK) {
-                    innerRight[n] = i;
-                    outerLeft[n] = i - nu[n];
-                }
+        for (int n = bas.length - 1; n >= 0; n--) {
+            glueingFromRight(bas[n]);
+        }
+    }
+
+    private void glueingFromLeft(BlackArea ba) {
+        // von unten - TODO ist vermutlich noch fehlerhaft
+        for (int i = ba.getOuterLeft(); i < ba.getInnerLeft(); i++) {
+            if (output[i] == BLACK) {
+                ba.setInnerLeft(i);
+                return;
             }
         }
     }
 
-    private void useTechniqueMercury() {
-        for (int i = 0; i < output.length; i++) {
-            if (output[i] == BLACK) {
-
+    private void glueingFromRight(BlackArea ba) {
+        // von oben - TODO ist vermutlich noch fehlerhaft
+        for (int i = ba.getOuterRight(); i > ba.getInnerRight(); i--) {
+            if (output[i - 1] == BLACK) {
+                ba.setInnerRight(i);
+                return;
             }
         }
     }
 
     private void setDefiniteWhite() {
-        // shortcut for convenience
-        if (nu.length == 0) {
-            Arrays.fill(output, WHITE);
-            return;
-        }
-        // Ermittle alles, was nicht durch Schwarz erreicht werden kann
+        // Ermittle in tmp alles, was nicht durch Schwarz erreicht werden kann
         char[] tmp = new char[output.length];
         Arrays.fill(tmp, UNKNOWN);
-        for (int n = 0; n < nu.length; n++) {
-            for (int j = outerLeft[n]; j < outerRight[n]; j++) {
-                tmp[j] = BLACK;
+        for (BlackArea ba : bas) {
+            for (int i = ba.outerLeft; i < ba.getOuterRight(); i++) {
+                tmp[i] = BLACK;
             }
         }
         // Alles, was nicht erreicht wird, muss weiß sein
-        for (int j = 0; j < output.length; j++) {
-            if (tmp[j] == UNKNOWN) {
-                output[j] = WHITE;
+        for (int i = 0; i < output.length; i++) {
+            if (tmp[i] == UNKNOWN) {
+                output[i] = WHITE;
             }
         }
     }
 
     private void setDefiniteBlack() {
-        // Sicher schwarze setzen
-        for (int i = 0; i < nu.length; i++) {
-            if (innerRight[i] - innerLeft[i] > nu[i]) {
-                throw new IllegalArgumentException("nicht möglich");
-            }
-            for (int j = innerLeft[i]; j < innerRight[i]; j++) {
-                output[j] = BLACK;
+        for (BlackArea ba : bas) {
+            for (int i = ba.getInnerLeft(); i < ba.getInnerRight(); i++) {
+                output[i] = BLACK;
             }
         }
     }
